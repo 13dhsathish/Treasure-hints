749603f1335bc6aa854590ef0c19deb60a624755
13dhsathish <76178573+13dhsathish@users.noreply.github.com>
2021-06-10 01:51:41 +0530

    Add files via upload

---
+++ Treasure hunt/js/Code.js
@@ -0,0 +1,24 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+    fill("white")
+    textSize(15)
+    text("A T A D A S B A", 500,200)
+    fill("lightblue")
+    text("Hint: Store the information in the server !!", 500,250)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 700,300)
+    fill("lightblue")
+    text("Hint: It performes a given/instructed task !!", 700,350)
+}
